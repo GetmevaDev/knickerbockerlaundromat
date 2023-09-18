831ad8699f7fbe9bed625eb45b4c756f9109b8c9
GetmevaDev <devteam@getmeva.com>
2023-09-18 15:19:32 +0600

    remove section

--- src/pages/laundry-pickup-and-delivery.js
+++ src/pages/laundry-pickup-and-delivery.js
@@ -19,7 +19,7 @@ export default function PickUp() {
 
       {/* <Description /> */}
       <SimpleTodo />
-      <Offer />
+      {/* <Offer /> */}
       {/* <EquipmentSection /> */}
     </Layout>
   )
