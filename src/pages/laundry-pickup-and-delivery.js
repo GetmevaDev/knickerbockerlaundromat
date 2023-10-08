66e88bea0f4efbacb0e3acff09f6ce9bb14168ed
GetmevaDev <devteam@getmeva.com>
2023-10-08 12:23:58 +0700

    text

--- src/pages/laundry-pickup-and-delivery.js
+++ src/pages/laundry-pickup-and-delivery.js
@@ -1,18 +1,17 @@
 import React from "react"
-import Layout from "../components/layout"
-import SectionHeaderLaundry from "../components/homePageComponents/sectionHeaderLaundry"
-import EquipmentSection from "../components/homePageComponents/equipmentSection/EquipmentSection"
-import SimpleTodo from "../components/SimpleTodo"
-import Offer from "../components/OfferLaundry"
-import { Description } from "../components/Description/Description"
-import { ThingsToKnow } from "../components/ThingsToKnow/ThingsToKnow"
 import { Areas } from "../components/Areas/Areas"
+import { Discount } from "../components/Discount/Discount"
+import SimpleTodo from "../components/SimpleTodo"
 import { TablePricing } from "../components/TablePricing/TablePricing"
+import { ThingsToKnow } from "../components/ThingsToKnow/ThingsToKnow"
+import SectionHeaderLaundry from "../components/homePageComponents/sectionHeaderLaundry"
+import Layout from "../components/layout"
 
 export default function PickUp() {
   return (
     <Layout>
       <SectionHeaderLaundry />
+      <Discount />
       <Areas />
       <TablePricing />
       <ThingsToKnow />
